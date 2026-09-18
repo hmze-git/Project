ff2b8dacbb5ce4fd2ae54e6038e5bbbe8c27abda
hmze-git <hamzahrassul92@gmail.com>
2026-09-18 23:55:19 +0200

    preprocess

--- Preprocess.py
+++ Preprocess.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-import cv2
+import cv2 as cv
 import os
+import matplotlib.pyplot as plt
 DatasetPath=r"C:\Users\Hamzah\Desktop\HYP\AAI\archive\dataset"
 
 dimX=224
@@ -15,11 +16,36 @@ def processDataset():
             os.makedirs(f"{str(retType.resolve())}(processed)",exist_ok=True)
             for retImg in retType.iterdir():
 
-                    img = cv2.imread(str(retImg.resolve()))
-                
-                    resizeImage=cv2.resize(img,(dimX,dimY))
-                
-                    cv2.imwrite(f'{retType.resolve()}(processed)/{retImg.name}.png',resizeImage)
+                    img = cv.imread(str(retImg.resolve()))
+
+
+                    resizeComp=cv.resize(img,(dimX,dimY))
+
+                    #Greyscale to threshold with a min max range
+                    grey=cv.cvtColor(img,cv.COLOR_RGB2GRAY)
+
+                    _,mask=cv.threshold(grey,10,255,cv.THRESH_BINARY)
+
+                    #usually do bitwise and with 2 images doing with same img gives img so pointless 
+                    #but needed so it wont break
+                    #mask does heavy lifting
+                    maskedImage=cv.bitwise_and(img,img,mask=mask)
+
+                    maskedImage=cv.medianBlur(maskedImage,3)
+
+                    resizedMask=cv.resize(maskedImage,(dimX,dimY))
+                    resizeImage=cv.resize(img,(dimX,dimY))
+
+                    
+                    plt.figure()
+                    plt.subplot(121)
+                    plt.imshow(resizedMask)
+                    plt.subplot(122)
+                    plt.imshow(resizeComp)
+
+                    plt.show()
+    
+                    #cv.imwrite(f'{retType.resolve()}(processed)/{retImg.name}.png',resizeImage)
 
 
 processDataset()
