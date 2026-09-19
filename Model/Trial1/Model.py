c1d84d07ea714d571603303111b48bd55b425af2
hmze-git <hamzahrassul92@gmail.com>
2026-09-19 18:51:17 +0200

    MLP based VAE for reconstructing MNIST images

--- Model/Trial1/Model.py
+++ Model/Trial1/Model.py
@@ -1,1 +1,60 @@
 import torch
+from torch import nn
+from torch.distributions import normal
+
+#Steps: Input->hiddenDim->mean,std->ReparamTrick->Decoder->OuputImage
+class VariationalAutoEncoder(nn.Module):
+
+    def __init__(self,inputDim,hiddenDim=200,zDim=20):
+        super().__init__()
+
+        #input layer to take 
+        self.img2Hd=nn.Linear(inputDim,hiddenDim)
+        self.hd2Mu=nn.Linear(hiddenDim,zDim)
+        self.hd2Sigma=nn.Linear(hiddenDim,zDim)
+
+        #decoder
+        self.z2Hid=nn.Linear(zDim,hiddenDim)
+        self.hid2Img=nn.Linear(hiddenDim,inputDim)
+
+        self.relu=nn.ReLU() 
+
+    def encode(self,x):
+        h=self.relu(self.img2Hd(x))
+        mu=self.hd2Mu(h)
+        sigma=self.hd2Sigma(h)
+        return mu,sigma
+
+    def decode(self,z):
+        h=self.relu(self.z2Hid(z))
+
+        imgRet=torch.sigmoid(self.hid2Img(h))
+        return imgRet
+
+    def forward(self,x):
+        mu,sigma=self.encode(x)
+        epsilon=torch.randn_like(sigma)
+
+        #epsiolon for intorducing stochasticity while removing it from netowrk so backprop can flow expectedly
+        # and we dont have some random value that is unaccounted for that may be causing shifts and not being adjusted accordingly
+        zReparam= mu+sigma*epsilon
+        xRecontstruct=self.decode(zReparam)
+        #Xrecon is for the MSE loss while mu and sigma is for the KL divergence(pushes to gaus)
+        return xRecontstruct,mu,sigma
+
+if __name__=="__main__":
+
+    device=None
+    if torch.cuda.is_available():
+        device= torch.device('cuda')
+        print(device)
+    x=torch.randn(4,28*28)
+    x=x.to(device)
+    vae=VariationalAutoEncoder(inputDim=784)
+    vae.to(device)
+    xRecon,mu,sigma=vae(x)
+
+    print(xRecon.shape)
+    print(mu.shape)
+    print(sigma.shape)
+    print(vae(x))
---
+++ Model/Trial1/run.py
@@ -0,0 +1,86 @@
+import torch
+import torchvision.datasets as datasets
+from tqdm import tqdm
+from Model import VariationalAutoEncoder
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+from torch import optim,nn
+
+
+#config
+
+DEVICE=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+INPUTDIM=784
+HDIM=200
+ZDIM=20 # Compresses the latent space
+LR=1e-4 #Karparthy constant
+BATCHSIZE=32
+
+NUMEPOCH=50
+
+
+#DATASER
+#transforms.tensor normalises 
+dataset=datasets.MNIST(root='dataset',train=True,transform=transforms.ToTensor(),download=True)
+trainLoader=DataLoader(dataset,batch_size=BATCHSIZE,shuffle=True)
+model=VariationalAutoEncoder(INPUTDIM,HDIM,ZDIM).to(DEVICE)
+optimiser=optim.Adam(model.parameters(),lr=LR)
+lossFn=nn.BCELoss(reduction="sum")
+
+#start Train
+
+for E in range(NUMEPOCH):
+    loop=tqdm(trainLoader)
+
+    for x,_ in loop:
+    #forward
+
+        x=x.to(DEVICE).view(x.shape[0],INPUTDIM)# Reshape to get input dim for Linear
+
+        xRecon,mu,sigma=model(x)
+
+        #push to recon imag
+        reconLoss=lossFn(xRecon,x)
+
+        #push towards std gause
+        klDiv=-0.5*torch.sum(1+torch.log(sigma.pow(2))-mu.pow(2)-sigma.pow(2))
+
+
+
+        #backprop
+        loss=reconLoss+klDiv
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+        loop.set_postfix(loss=loss.item()/x.shape[0])
+
+
+def inference(digit,numGen=5):
+    images=[]
+    idx=0
+    for x,y in dataset:
+        if y==idx:
+            x=x.to(DEVICE)
+            images.append(x)
+            idx+=1
+
+        if idx==10:
+            break
+    encodingDigit=[]
+    for d in range(10):
+        mu,sigman=model.encode(images[d].view(1,784))
+        encodingDigit.append((mu,sigman))
+
+    mu,sigma=encodingDigit[digit]
+
+    for ex in range(numGen):
+        epsilon=torch.randn_like(sigma)
+        z=mu+sigma*epsilon
+        out=model.decode(z)
+        out=out.view(-1,1,28,28)
+        save_image(out,f"Generated_{digit}_ex{ex}.png")
+
+
+for idx in range(10):
+    inference(idx,5)
