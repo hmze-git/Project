febd2ff72d382083a19e9a54e00cd8df75ccbd27
hmze-git <hamzahrassul92@gmail.com>
2026-09-22 00:29:35 +0200

    recon done

--- Model/VAE.py
+++ Model/VAE.py
@@ -13,12 +13,23 @@ class AnomalyVariationalAutoEncoder(nn.Module):
         self.encoder=Encoder(inputChannels=inputChannels,latentDim=latenDim,rdim=rdim)
         self.decoder=Decoder(inputChannels=inputChannels,latentDim=latenDim,rdim=rdim,shapeLatent=(32,7,7))
 
-    def encoder(self,x):
-        mu,sigma=self.encoder(x)
-   
+    def encoding(self,x):
+        zParam,mu,logvar=self.encoder(x)
+        return zParam,mu,logvar
+        
 
-    def decoder(self,z):
-        pass
+    def decoding(self,zReparam):
+        xReconstructed=self.decoder(zReparam) 
+
+        return xReconstructed
+
+    def forward(self,x):
+
+        zReparam,mu,logVar=self.encoding(x)
+
+        xRecon=self.decoding(zReparam)
+
+        return xRecon,mu,logVar
 
     
     
